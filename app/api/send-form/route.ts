11acb1db2d814f70a593a7c9a79dbd616194da52
fiberon <test@gmail.com>
2025-03-31 10:24:15 +0200

    Fix email

--- app/api/send-form/route.ts
+++ app/api/send-form/route.ts
@@ -1,26 +1,26 @@
 import { NextResponse } from 'next/server';
 import { Resend } from 'resend';
-// const resend = new Resend(process.env.RESEND_API_KEY);
-import * as fs from 'fs/promises';
+const resend = new Resend(process.env.RESEND_API_KEY);
+// import * as fs from 'fs/promises';
 
 export async function POST(request: Request) {
   try {
     const { pdfBlob, fullName } = await request.json();
     console.log(`${fullName}-${new Date().toISOString().split('T')[0]}.pdf`);
 
-    fs.writeFile(`./public/${fullName}-${new Date().toISOString().split('T')[0]}.pdf`, Buffer.from(pdfBlob, 'base64'));
-    // await resend.emails.send({
-    //   from: 'onboarding@resend.dev',
-    //   to: "openko.dental@gmail.com",
-    //   subject: 'Medical Form Submission',
-    //   text: 'Please find your medical form attached.',
-    //   attachments: [
-    //     {
-    //       filename: `${fullName}-${new Date().toISOString().split('T')[0]}.pdf`,
-    //       content: Buffer.from(pdfBlob, 'base64'),
-    //     },
-    //   ],
-    // });
+    // fs.writeFile(`./public/${fullName}-${new Date().toISOString().split('T')[0]}.pdf`, Buffer.from(pdfBlob, 'base64'));
+    await resend.emails.send({
+      from: 'onboarding@resend.dev',
+      to: "openko.dental@gmail.com",
+      subject: 'Medical Form Submission',
+      text: 'Please find your medical form attached.',
+      attachments: [
+        {
+          filename: `${fullName}-${new Date().toISOString().split('T')[0]}.pdf`,
+          content: Buffer.from(pdfBlob, 'base64'),
+        },
+      ],
+    });
 
     return NextResponse.json({ success: true });
   } catch (error) {
